b1360a7971d291427be01309ec368133629a6bb7
lcn <lcniell@gmail.com>
2024-05-02 10:49:10 -0700

    axios performance improvement

--- src/VideoUpload.js
+++ src/VideoUpload.js
@@ -1,100 +1,144 @@
 import React, { useState, useRef, useEffect } from "react";
 import { useDropzone } from "react-dropzone";
-// get our fontawesome imports
-import { faFolderOpen, faHome } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faFolderOpen } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
 function VideoUpload() {
   const [videoURL, setVideoURL] = useState("");
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
-  const [fileName, setFileName] = useState("");
   const [thumbnails, setThumbnails] = useState([]);
-  const [currentThumbnailIndex, setCurrentThumbnailIndex] = useState(0);
   const [sliderPosition, setSliderPosition] = useState(0);
   const [isDragging, setIsDragging] = useState(false);
+  const [videoTitle, setVideoTitle] = useState(""); // State to hold the video title
+  const [currentTimeDisplay, setCurrentTimeDisplay] = useState("00:00");
+  const [loading, setLoading] = useState(true); // State to handle the loading condition
+
   const timelineRef = useRef(null);
+  const formatTime = (timeInSeconds) => {
+    const pad = (num) => (num < 10 ? "0" + num : num);
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = Math.floor(timeInSeconds % 60);
+    return `${pad(minutes)}:${pad(seconds)}`;
+  };
 
   useEffect(() => {
     if (videoURL && videoRef.current) {
       videoRef.current.addEventListener("loadedmetadata", () => {
         generateThumbnails();
-        updateSliderPosition(0); // Initialize the slider at the start when metadata is loaded
+        updateSliderPosition(0);
       });
     }
   }, [videoURL]);
 
-  const generateThumbnails = () => {
-    const numThumbnails = 50; // Adjust the number of thumbnails based on your needs
-    const duration = videoRef.current.duration;
-    let tempThumbnails = [];
-    let count = 0; // Counter to track when all thumbnails have been processed
+  const generateThumbnails = (videoSrc) => {
+    const video = document.createElement("video");
+    video.src = videoSrc;
 
-    const captureFrame = (time, index) => {
-      videoRef.current.currentTime = time;
-      videoRef.current.addEventListener(
-        "seeked",
-        function onSeeked() {
+    video.addEventListener("loadedmetadata", () => {
+      const duration = video.duration;
+      const interval = duration / 50; // More thumbnails for smoother transitions
+      let currentTime = 0;
+
+      const updateThumbnail = () => {
+        if (currentTime <= duration) {
           const canvas = canvasRef.current;
+          canvas.width = 120;
+          canvas.height = 68;
           const context = canvas.getContext("2d");
+          context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+          const newThumbnail = { src: canvas.toDataURL(), time: currentTime };
+          setThumbnails((prevThumbnails) => [...prevThumbnails, newThumbnail]);
+
+          currentTime += interval;
+          video.currentTime = currentTime;
+        } else {
+          video.removeEventListener("seeked", updateThumbnail);
+        }
+      };
+
+      video.addEventListener("seeked", updateThumbnail);
+      video.currentTime = currentTime;
+    });
+  };
+
+  const captureFrame = (time) => {
+    return new Promise((resolve, reject) => {
+      if (!videoRef.current) {
+        console.error("Video element is not available");
+        resolve(null);
+        return;
+      }
+      // Log the intended seek time.
+      console.log(`Seeking to time: ${time}`);
+
+      const onSeeked = () => {
+        console.log(
+          `Video has seeked to time: ${videoRef.current.currentTime}`
+        );
+        try {
+          if (!canvasRef.current) {
+            console.error("Canvas element is not available");
+            resolve(null);
+            return;
+          }
+          const context = canvasRef.current.getContext("2d");
+          context.clearRect(
+            0,
+            0,
+            canvasRef.current.width,
+            canvasRef.current.height
+          );
           context.drawImage(
             videoRef.current,
             0,
             0,
-            canvas.width,
-            canvas.height
+            canvasRef.current.width,
+            canvasRef.current.height
           );
-          tempThumbnails[index] = {
-            src: canvas.toDataURL("image/jpeg"),
-            time: time,
-          };
-          count++;
-          if (count === numThumbnails) {
-            // Ensure we sort by time in case the 'seeked' events finish out of order
-            setThumbnails(tempThumbnails.sort((a, b) => a.time - b.time));
-          }
+          const src = canvasRef.current.toDataURL("image/jpeg");
           videoRef.current.removeEventListener("seeked", onSeeked);
-        },
-        { once: true }
-      );
-    };
+          resolve({ src, time });
+        } catch (error) {
+          console.error("Error capturing frame:", error);
+          resolve(null);
+        }
+      };
 
-    for (let i = 0; i < numThumbnails; i++) {
-      const time = duration * (i / (numThumbnails - 1)); // Ensure the last thumbnail is at the very end
-      captureFrame(time, i);
-    }
-  };
-
-  const calculateThumbnailOpacity = (index) => {
-    const currentIndex = Math.round(
-      sliderPosition / (timelineRef.current.offsetWidth / thumbnails.length)
-    );
-    const distance = Math.abs(index - currentIndex);
-    if (distance > 3) {
-      return 0.1;
-    }
-    return 1 - (distance / 3) * 0.9; // From 1 to 0.1 within 3 frames
+      videoRef.current.addEventListener("seeked", onSeeked, { once: true });
+      videoRef.current.currentTime = time; // Update currentTime after adding the event listener.
+    });
   };
 
   const onDrop = (acceptedFiles) => {
     const file = acceptedFiles[0];
-    setFileName(file.name)
     const url = URL.createObjectURL(file);
     setVideoURL(url);
-    updateSliderPosition(0); // Reset slider to the beginning when a new video is loaded
+    generateThumbnails(url);
+
+    const formData = new FormData();
+    formData.append("video", file);
+    axios
+      .post("http://localhost:5000/upload", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+      .catch((error) => console.error("Error uploading video", error));
   };
+
+  useEffect(() => {
+    if (videoURL && videoRef.current) {
+      videoRef.current.addEventListener("loadedmetadata", () => {
+        generateThumbnails(videoRef.current.duration);
+      });
+    }
+  }, [videoURL]);
 
   const onTimeUpdate = () => {
     if (!isDragging) {
       updateSliderPosition(videoRef.current.currentTime);
     }
-  };
-  const formatTime = (timeInSeconds) => {
-    const pad = (num, size) => num.toString().padStart(size, '0');
-    const seconds = Math.floor(timeInSeconds % 60);
-    const minutes = Math.floor((timeInSeconds / 60) % 60);
-    return `${pad(minutes, 2)}:${pad(seconds, 2)}`;
   };
 
   const updateSliderPosition = (currentTime) => {
@@ -103,16 +147,9 @@ function VideoUpload() {
         (currentTime / videoRef.current.duration) *
         timelineRef.current.offsetWidth;
       setSliderPosition(newPosition);
-      // Also update the displayed time
-      const timeDisplay = document.getElementById('time-display');
-      if (timeDisplay) {
-        timeDisplay.textContent = formatTime(currentTime);
-        timeDisplay.style.left = `${newPosition - timeDisplay.offsetWidth / 2}px`; // Center the time display over the slider
-      }
     }
   };
 
-  // Handle mouse events for dragging the slider
   const handleMouseMove = (event) => {
     if (isDragging && timelineRef.current) {
       const rect = timelineRef.current.getBoundingClientRect();
@@ -129,7 +166,7 @@ function VideoUpload() {
 
   const handleMouseDown = (event) => {
     setIsDragging(true);
-    handleMouseMove(event); // Update immediately on click
+    handleMouseMove(event);
   };
 
   const handleMouseUp = () => {
@@ -143,83 +180,108 @@ function VideoUpload() {
 
   return (
     <div
-      style={{ background: "black", color: "white", padding: "10px" }}
+      style={{
+        background: "black",
+        color: "white",
+        padding: "10px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
       onMouseLeave={handleMouseUp}
     >
-      {fileName}
+      {!videoURL && (
+        <div
+          {...getRootProps()}
+          style={{
+            border: "2px dashed white",
+            padding: 40,
+            textAlign: "center",
+            cursor: "pointer",
+            width: "50%", // Ensuring it doesn't take full width
+            marginBottom: "20px",
+          }}
+        >
+          <input {...getInputProps()} />
+          {isDragActive ? (
+            <p>Drop the video here...</p>
+          ) : (
+            <p>
+              Drag 'n' drop a video here, or click to select a video
+              <FontAwesomeIcon icon={faFolderOpen} />
+            </p>
+          )}
+        </div>
+      )}
       {videoURL && (
-        <div>
+        <div style={{ width: "50%" }}>
+          {" "}
+          {/* Container for video and thumbnails */}
           <video
             ref={videoRef}
             src={videoURL}
-            width="100%"
-            controls
+            //controls
             onTimeUpdate={onTimeUpdate}
-            style={{ display: "block", margin: "auto", background: "black" }}
+            style={{
+              display: "block",
+              width: "100%",
+              borderRadius: "8px", // Increased border radius
+            }}
           />
           <div
-  ref={timelineRef}
-  className="timeline"
-  style={{
-    position: "relative",
-    width: "100%",
-    height: "90px",
-    overflowX: "hidden",
-    cursor: "pointer",
-    marginTop: "20px",
-  }}
-  onMouseDown={handleMouseDown}
->
-  {thumbnails.map((thumbnail, index) => (
-    <div
-      key={index}
-      style={{
-        position: "absolute",
-        width: `${100 / thumbnails.length}%`,
-        height: "100%",
-        left: `${(100 / thumbnails.length) * index}%`,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <img
-        src={thumbnail.src}
-        alt={`Thumbnail ${index}`}
-        style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-          objectPosition: "center",
-          opacity: calculateThumbnailOpacity(index),
-        }}
-        onClick={() => (videoRef.current.currentTime = thumbnail.time)}
-      />
-    </div>
-  ))}
-  <div
-    style={{
-      position: "absolute",
-      top: "0",
-      left: `${sliderPosition}px`,
-      height: "90px",
-      width: "5px",
-      backgroundColor: "red",
-    }}
-  />
-  <span
-    id="time-display"
-    style={{
-      position: "absolute",
-      top: "-20px", // Adjust as necessary to position the time display above the slider
-      left: `${sliderPosition}px`,
-      color: "white",
-      whiteSpace: "nowrap", // Ensure the text does not wrap
-    }}
-  >{formatTime(videoRef.current ? videoRef.current.currentTime : 0)}</span>
-</div>
+            ref={timelineRef}
+            style={{
+              position: "relative",
+              width: "100%",
+              height: "45px",
+              overflowX: "hidden",
+              cursor: "pointer",
+              marginTop: "20px",
+              borderRadius: "8px", // Increased border radius
+              display: "flex",
+            }}
+            onMouseDown={handleMouseDown}
+          >
+            {thumbnails.map((thumbnail, index) => (
+              <div
+                key={index}
+                style={{
+                  flex: "1 1 auto",
+                  height: "100%",
+                  position: "relative",
+                }}
+              >
+                <img
+                  src={thumbnail.src}
+                  alt={`Thumbnail ${index}`}
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover",
+                    objectPosition: "center",
+                  }}
+                  onClick={() =>
+                    (videoRef.current.currentTime = thumbnail.time)
+                  }
+                />
+              </div>
+            ))}
+            <div
+              style={{
+                position: "absolute",
+                top: "0",
+                left: `${sliderPosition}px`,
+                height: "90px",
+                width: "5px",
+                backgroundColor: "white",
+                zIndex: 3,
+              }}
+            />
+          </div>
         </div>
       )}
       <canvas
@@ -228,18 +290,6 @@ function VideoUpload() {
         width="160"
         height="90"
       />
-      <div
-        {...getRootProps()}
-        style={{ border: "2px dashed white", padding: 40, textAlign: "center" }}
-      >
-        <input {...getInputProps()} />
-        {isDragActive ? (
-          <p>Add Video here ...</p>
-        ) : (
-          <p>Add Video       <FontAwesomeIcon icon={faFolderOpen} />
-          </p>
-        )}
-      </div>
     </div>
   );
 }
